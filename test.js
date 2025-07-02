c96be0ca05aaa89b1492bd49105e421e9ff3fed0
acidbear55 <annabelflett@gmail.com>
2025-07-02 15:51:47 +0200

    basic drag and drop with words

--- test.js
+++ test.js
@@ -1,37 +1,13 @@
-function setMessage() {
-    const select = document.getElementById("options");
-    const message = document.getElementById("message");
-    const value = select.value;
-    if (value) {
-        message.textContent = "Selection: " + value;
-    } else {
-        message.textContent = "No Selection Yet";
-    }
-}
+function allowDrop (ev)  { 
+    ev.preventDefault (); 
+    } 
 
-function drawGraph() {
-    var trace1 = {
-  x: [1, 2, 3, 4],
-  y: [10, 15, 13, 17],
-  mode: 'markers',
-  type: 'scatter'
-};
+function drag (ev)  { 
+    ev.dataTransfer.setData( 'text',ev.target.id ); 
+    } 
 
-var trace2 = {
-  x: [2, 3, 4, 5],
-  y: [16, 5, 11, 9],
-  mode: 'lines',
-  type: 'scatter'
-};
-
-var trace3 = {
-  x: [1, 2, 3, 4],
-  y: [12, 9, 15, 12],
-  mode: 'lines+markers',
-  type: 'scatter'
-};
-
-var data = [trace1, trace2, trace3];
-
-//Plotly.newPlot('myDiv', data);
-}
+function  drop (ev)  { 
+    ev.preventDefault (); 
+    let data  =  ev.dataTransfer.getData('text'); 
+    ev.target.appendChild(document.getElementById(data)); 
+    } 
