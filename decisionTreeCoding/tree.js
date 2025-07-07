98b7fc722faa893a0ab8d9e8dd38745bc553bce1
1010happy <yilin.zhao.1010@gmail.com>
2025-07-07 14:26:50 +0200

    selectionof nodes

--- decisionTreeCoding/tree.js
+++ decisionTreeCoding/tree.js
@@ -1,4 +1,56 @@
 ///ddd = drag dropdown direct
+const BLOCKTYPE_BODY= 0;
+const BLOCKTYPE_END = 1;
+const BLOCKTYPE_START = 2; 
+
+const NODETYPE_PARENT = 3;
+const NODETYPE_YES = 4;
+const NODETYPE_NO = 5;
+
+const HIGHLIGHT = "3px";
+const NOHIGHLIGHT = "1px"
+
+let curWaiting = null;
+let connections = [];
+
+class Node{
+  constructor(parent_id, type){
+    this.type = type;
+
+    this.parent_id = parent_id;
+    if(type == NODETYPE_PARENT)this.id = (parent_id + "_parent");
+    else if (type == NODETYPE_YES) this.id = (parent_id + "_yes");
+    else this.id = (parent_id + "_no");
+
+    this.element = document.getElementById(this.id);
+    this.isSelected = false;
+
+    this.partner = null;
+  }
+}
+
+class block {
+  constructor(id, type) {
+    this.id = "";
+    this.type = type;
+    this.element = document.getElementById(id);
+    this.moverElement = document.getElementById(id + "_mover");
+
+    this.parentNode = new Node (id, NODETYPE_PARENT); 
+    this.noNode = new Node (id, NODETYPE_NO); 
+    this.yesNode = new Node (id, NODETYPE_YES); 
+
+    if(type == BLOCKTYPE_BODY){
+      this.optionsArray = [("Option1"+id), ("Option2" + id)];
+    }else if (type == BLOCKTYPE_START){
+      this.optionsArray = ["Option.Start1", "Option Start2"];
+    }else if (type == BLOCKTYPE_END){
+      this.optionsArray = ["Option End1", "Option End2"];
+    }
+    
+  }
+}
+
 
 // let //can reval but not redeclare (var can be redeclared accidentally), const is ur normal let
 
@@ -21,20 +73,71 @@
 // }
 
 
-function ddd_init(id, hasParent = true, hasKids = true){
+function highlight_init(node){
+
+  //block.nodetype is selected = false and true
+  (node.element).addEventListener("mousedown", (e) => {
+    if(!node.isSelected){
+      if(curWaiting === null){
+        console.log("curWaiting set" + node.id);
+        curWaiting = node;
+        node.isSelected = true;
+        node.element.style.borderWidth = "10px";//HIGHLIGHT;
+
+      }else{ 
+        console.log(curWaiting.parent_id + " " + node.parent_id);
+        console.log(curWaiting.type + " " + node.type);
+        if((curWaiting.parent_id != node.parent_id) && //can match 
+         (((curWaiting.type === NODETYPE_PARENT) && (node.type === NODETYPE_NO || node.type === NODETYPE_YES)) || 
+          ((curWaiting.type === NODETYPE_NO || curWaiting.type === NODETYPE_YES) && node.type === NODETYPE_PARENT))){
+            console.log("paired" + node.id);
+            //valid match
+            node.isSelected = true;
+            node.partner = curWaiting;
+            node.element.style.borderWidth = HIGHLIGHT;
+
+            curWaiting.partner = node;
+            curWaiting = null;
+        }
+      }
+    } else { //node was selected
+      if(curWaiting == node){ //it is the waiting node
+        console.log("waiting undone" + node.id);
+        node.isSelected = false;
+        curWaiting = null;
+        node.element.style.borderWidth = NOHIGHLIGHT;
+
+      } else { //it was paired
+        console.log("paired" + node.id + node.partner.element.id);
+        node.element.style.borderWidth = NOHIGHLIGHT;
+        node.partner.element.style.borderWidth = NOHIGHLIGHT;
+
+        node.isSelected = false;
+        node.partner.isSelected = false;
+
+        node.partner.partner = null;
+        node.partner = null;
+      }
+    }
+  });
+}
+
+
+
+function ddd_init(block){
+  let hasParent = (block.type == BLOCKTYPE_START)? false : true;
+  let hasKids = (block.type == BLOCKTYPE_END)? false: true;
 
   // init dragging
-  const moveTarget = document.getElementById(id); // the whole thing
-  const dragTarget = document.getElementById(id+"_mover");
-  // const dragTarget = document.getElementById(id);
-
+  let moveTarget = block.element;//document.getElementById(id); // the whole thing
+  let dragTarget = block.moverElement;// document.getElementById(id+"_mover");
   let isDragging = false;
   let offsetX, offsetY;
 
   dragTarget.addEventListener("mousedown", (e) => {
     isDragging = true;
     offsetX = e.clientX - moveTarget.offsetLeft;//dragTarget.offsetLeft;
-    offsetY = e.clientY - moveTarget.offsetLeft;//dragTarget.offsetTop;
+    offsetY = e.clientY - moveTarget.offsetTop;//dragTarget.offsetTop;
     document.body.style.userSelect = "none";
   });
 
@@ -49,36 +152,22 @@ function ddd_init(id, hasParent = true, hasKids = true){
     isDragging = false;
     document.body.style.userSelect = "auto";
   });
-  
-  function highlight_init(target){
-    let isSelected = false;
-    target.addEventListener("mousedown", (e) => {
-      if(isSelected){ //disable highlighting
-        isSelected = false; // no longer selected
-        target.style.borderWidth = "1px";
-      }else{ //highlight
-        isSelected = true;
-        target.style.borderWidth = "10px";
-      }
-    });
-  }
 
   //init yes no parent selection
   if(hasParent){
-    const parentTarget = document.getElementById(id+"_parent");
-    highlight_init(parentTarget);
+    highlight_init(block.parentNode);
   }
 
   if(hasKids){
-    const yesTarget = document.getElementById(id+"_yes");
-    highlight_init(yesTarget);
-    const noTarget = document.getElementById(id+"_no");
-    highlight_init(noTarget);
+    highlight_init(block.noNode);
+    highlight_init(block.yesNode);
   }
 }
 
 //eg.
 // make_ddd("block1", ["Apples", "Bananas", "Oranges"]);
 // make_ddd("block2", ["Option A", "Option B", "Option C"]);
-ddd_init("block1");
-// ddd_init("block2");
+const block1 = new block("block1", BLOCKTYPE_BODY);
+ddd_init(block1);
+const block2 = new block("block2", BLOCKTYPE_BODY);
+ddd_init(block2);
