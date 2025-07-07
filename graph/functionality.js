1127e9c6d609da374ed338f9a028ecaac6fb71d2
acidbear55 <annabelflett@gmail.com>
2025-07-07 16:52:18 +0200

    added features

--- graph/functionality.js
+++ graph/functionality.js
@@ -1,61 +1,75 @@
 
+// general variables
+
+document.getElementById("degree").max = 0
+let penState = true
+let rubberState = false
+let addedPolynomial = false
+const degree_input = document.getElementById('degree')
+
+// button reponses
+
 function handleClick() {
     const degree = document.getElementById('degree').value;
     plotLeastSquares(degree)
     console.log(degree)
 }
 
+function penMode() {
+  penState = true
+  rubberState = false
+}
+
+function rubberMode() {
+  penState = false
+  rubberState = true
+}
+
 function plotLeastSquares(degree) {
     plotPolynomial(degree)
 }
 
+// event listeners 
 
-let input = document.getElementById('degree');
-input.addEventListener('keydown', function (e) {
+document.getElementById("submit_button").addEventListener("click", handleClick);
+document.getElementById("plot").addEventListener("click",alterDataPoint);
+document.getElementById("pen").addEventListener("click",penMode);
+document.getElementById("rubber").addEventListener("click",rubberMode);
+degree_input.addEventListener('keydown', function (e) {
     // only allow strictly numeric input
   if (["e", "E", "+", "-"].includes(e.key)) {
     e.preventDefault();
   }
+  // if (Number(e.key) > chart.data.datasets.length - 1) {
+  //   const p = document.createElement("p")
+  //   p.textContent = `<p> Please enter a degree less than ${chart.data.datasets.length + 1}</p>`
+  //   let parent = document.getElementById("submission")
+  //   console.log(parent)
+  //   parent.appendChild(p)
+  // }
+
+
 }
 )
 
-document.getElementById("submit_button").addEventListener("click", handleClick);
-
-document.getElementById("plot").addEventListener("click",addDataPoint);
-
-function addDataPoint() {
-    console.log("clicked")
-}
-
-
-
-const data_scatter = [
-    {x:4,y:3},
-    {x:-2,y:7},
-    {x:-5,y:9},
-    {x:7,y:17},
-    {x:5,y:4}
-  ];
-
+// define the chart 
 const chart = new Chart(
     document.getElementById('plot'),
     {
       type: 'scatter',
       data: {
-        labels: data_scatter.map(row => row.x),
-        datasets: [
-          {
-            label: 'data',
-            data: data_scatter.map(row => row.y)
-          }
-        ]
+        datasets: []
       },
       options: {
+        plugins : {
+          legend : {
+          display : false
+        }},
         events: [],
         scales: {
              x: {
                 type: 'linear',
-                    min: -10,  
+                    min: 0,  
                     max: 10,   
             },
             y: {
@@ -68,19 +82,64 @@ const chart = new Chart(
     }
   );
 
-function smallQ(Q,degree) {
-    let J = math.transpose(Q)
-    J = J.slice(0,(Number(degree)+1))
-    J = math.transpose(J)
-    return J
+// controlling datapoints functions
+function alterDataPoint(event) {
+
+    let x = chart.scales['x'].getValueForPixel(event.offsetX)
+    let y = chart.scales['y'].getValueForPixel(event.offsetY)
+
+    if (penState) {
+      chart.data.datasets.push({
+      data: [{x:x,y:y}],
+      borderColor: 'black',
+      backgroundColor : 'green',
+      fill: true,
+      pointRadius: 3,
+      borderWidth: 1,
+      })
+      } else if (rubberState) {
+        checkDeleting(x,y)
+      };
+
+    chart.update()
+    
+    if (!addedPolynomial) {
+    document.getElementById("degree").max = chart.data.datasets.length - 1;
+    } else {document.getElementById("degree").max = chart.data.datasets.length - 2}
+
 }
 
-function polyCalc(x,degree) {
-    // need to make this not hardcoded, takes data from the graph
-    let m = []
-    let x_data = [4,-2,-5,7,5]
+function checkDeleting(x,y) {
+  let pointsToRemove = []
+  chart.data.datasets.forEach((item,i) => {
+      let x_plotted = item.data['0'].x
+      let y_plotted = item.data['0'].y
+      const dist = Math.hypot(x-x_plotted,y-y_plotted)
+      // console.log(dist)
+      if (dist < 0.5) {
+        pointsToRemove.push(i)
+      }
+  })
+    pointsToRemove.forEach((item) => {
+      console.log(item)
+      chart.data.datasets.splice(item,1)
+  })
+}
 
-    for (let i =0; i<=4; i++){
+
+// polynomial plotting functions
+function polyCalc(x,degree) {
+    let x_data = []
+    let y_data = []
+    let m = []
+
+    for (let p=0; p<chart.data.datasets.length; p++) {
+      x_data.push(chart.data.datasets[p].data['0'].x)
+      y_data.push(chart.data.datasets[p].data['0'].y)
+    }
+
+
+    for (let i =0; i<x_data.length; i++){
         let number = x_data[i]
         var new_row = []
         for (let j = 0; j <= degree; j++) {
@@ -89,12 +148,12 @@ function polyCalc(x,degree) {
         m.push(new_row)
     }
 
-    const y = [3,7,8,17,4] 
     const result = math.qr(m) // returns full QR so we need to convert Q/R matrices
     const R = result.R.slice(0,(Number(degree)+1))
     let Q = smallQ(result.Q,degree)
-    let coefficients = math.multiply(math.inv(R),math.multiply(math.transpose(Q),y))
+    let coefficients = math.multiply(math.inv(R),math.multiply(Q,y_data))
     console.log(coefficients)
+
     let total = 0;
     let exponented = 1;
     for (let k = 0; k<=degree; k++) {
@@ -105,21 +164,40 @@ function polyCalc(x,degree) {
 }
 
 function plotPolynomial(degree) {
+    addedPolynomial = true
+    removePolynomial()
     const dataPoints = [];
     for (let x = -10; x <= 10; x+=0.05) {
     dataPoints.push({ x: x, y: polyCalc(x,degree) });
     }
 
     chart.data.datasets.push({
+    label : "polynomial",
     showLine : true,
     data: dataPoints,
-    borderColor: 'green',
-    fill: false,
+    borderColor: 'red',
     pointRadius: 0,
-    borderWidth: 2,
     });
 
     chart.update()
 };
+
+
+// utilities
+
+function removePolynomial() {
+  chart.data.datasets = chart.data.datasets.filter(
+    dataset => dataset.label !== "polynomial"
+  )
+
+  chart.update()
+}
+
+function smallQ(Q,degree) {
+  // note returns Q^T (from original [Q Q_orth])
+    let J = math.transpose(Q)
+    J = J.slice(0,(Number(degree)+1))
+    return J
+}
 
 
